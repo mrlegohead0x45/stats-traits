740260149c66f5e6b7aaba1e53e0e9c2f480888e
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-11-30 17:33:16 +0000

    Add docs

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"stats":{"doc":"","t":[8,11,11,11,11],"n":["Stats","checked_mean","count","mean","sum"],"q":["stats","","","",""],"d":["A trait to be implemented for collection-like types that …","Like <code>Stats::mean</code>, but returns <code>None</code> if the collection is …","Count the items in the collection","Find the mean of the collection","Calculate the sum of all the items in the collection"],"i":[0,3,3,3,3],"f":[0,[[],1],[[],2],[[]],[[]]],"p":[[4,"Option"],[15,"usize"],[8,"Stats"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"stats":["",[],["lib.rs"]]\
+}');
+createSourceSidebar();
